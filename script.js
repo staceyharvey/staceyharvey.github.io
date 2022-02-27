66fec3b19cebda5869931b3acf7408a3d9a7978e
staceyharvey <97730042+staceyharvey@users.noreply.github.com>
2022-02-27 18:13:17 +0000

    y

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ----------------------------------------TSHIRTS--------------------------------------------------
 
-var tshirt
-    // Reset the tshirts back to white
+var tshirt;
+// Reset the tshirts back to white
 function reset() {
     document.getElementById("xs").src = "../images/Blue.png";
     document.getElementById("s").src = "../images/Green.png";
@@ -28,7 +28,20 @@ document.getElementById("form").addEventListener("submit", function(event) {
     var automation = document.getElementById("automation-output").innerHTML;
     var infra = document.getElementById("infra-output").innerHTML;
     // Take all the slider values and add them together (answer converts to days)
-    var total = parseFloat((parseFloat(complexity) + parseFloat(uncertainty) + parseFloat(security) + parseFloat(controls) + parseFloat(familiarity) + parseFloat(quality) + parseFloat(dependencies) + parseFloat(performance) + parseFloat(manual) + parseFloat(perf) + parseFloat(automation) + parseFloat(infra)));
+    var total = parseFloat(
+        parseFloat(complexity) +
+        parseFloat(uncertainty) +
+        parseFloat(security) +
+        parseFloat(controls) +
+        parseFloat(familiarity) +
+        parseFloat(quality) +
+        parseFloat(dependencies) +
+        parseFloat(performance) +
+        parseFloat(manual) +
+        parseFloat(perf) +
+        parseFloat(automation) +
+        parseFloat(infra)
+    );
     // Determine which tshirt to highlight based on the total above
     if (total <= 5) {
         tshirt = "xs";
@@ -41,23 +54,23 @@ document.getElementById("form").addEventListener("submit", function(event) {
     } else if (total > 20) {
         tshirt = "xl";
     } else {
-        alert("Oops! Stacey messed up the code!")
-    };
+        alert("Oops! Stacey messed up the code!");
+    }
     reset();
     // Change the colour of the relevant tshirt
     if (tshirt == "xs") {
-        document.getElementById("xs").src = "../images/Blue2.png"
+        document.getElementById("xs").src = "../images/Blue2.png";
     } else if (tshirt == "s") {
-        document.getElementById("s").src = "../images/Green2.png"
+        document.getElementById("s").src = "../images/Green2.png";
     } else if (tshirt == "m") {
-        document.getElementById("m").src = "../images/Yellow2.png"
+        document.getElementById("m").src = "../images/Yellow2.png";
     } else if (tshirt == "l") {
-        document.getElementById("l").src = "../images/Orange2.png"
+        document.getElementById("l").src = "../images/Orange2.png";
     } else if (tshirt == "xl") {
-        document.getElementById("xl").src = "../images/Red2.png"
+        document.getElementById("xl").src = "../images/Red2.png";
     } else {
-        alert("Oops! Stacey messed up the code!")
-    };
+        alert("Oops! Stacey messed up the code!");
+    }
 });
 
 // A = 0.5
@@ -69,18 +82,42 @@ document.getElementById("form").addEventListener("submit", function(event) {
 
 // ----------------------------------------LISTENERS--------------------------------------------------
 
-document.getElementById("complexity").addEventListener("change", function() { thumbFFunction.call(this, "complexity"); });
-document.getElementById("uncertainty").addEventListener("change", function() { thumbFFunction.call(this, "uncertainty"); });
-document.getElementById("security").addEventListener("change", function() { thumbCFunction.call(this, "security"); });
-document.getElementById("controls").addEventListener("change", function() { thumbCFunction.call(this, "controls"); });
-document.getElementById("unfamiliarity").addEventListener("change", function() { thumbDFunction.call(this, "unfamiliarity"); });
-document.getElementById("quality").addEventListener("change", function() { thumbFFunction.call(this, "quality"); });
-document.getElementById("dependencies").addEventListener("change", function() { thumbCFunction.call(this, "dependencies"); });
-document.getElementById("performance").addEventListener("change", function() { thumbDFunction.call(this, "performance"); });
-document.getElementById("manual").addEventListener("change", function() { thumbBFunction.call(this, "manual"); });
-document.getElementById("perf").addEventListener("change", function() { thumbAFunction.call(this, "perf"); });
-document.getElementById("automation").addEventListener("change", function() { thumbEFunction.call(this, "automation"); });
-document.getElementById("infra").addEventListener("change", function() { thumbBFunction.call(this, "infra"); });
+document.getElementById("complexity").addEventListener("change", function() {
+    thumbFFunction.call(this, "complexity");
+});
+document.getElementById("uncertainty").addEventListener("change", function() {
+    thumbFFunction.call(this, "uncertainty");
+});
+document.getElementById("security").addEventListener("change", function() {
+    thumbCFunction.call(this, "security");
+});
+document.getElementById("controls").addEventListener("change", function() {
+    thumbCFunction.call(this, "controls");
+});
+document.getElementById("unfamiliarity").addEventListener("change", function() {
+    thumbDFunction.call(this, "unfamiliarity");
+});
+document.getElementById("quality").addEventListener("change", function() {
+    thumbFFunction.call(this, "quality");
+});
+document.getElementById("dependencies").addEventListener("change", function() {
+    thumbCFunction.call(this, "dependencies");
+});
+document.getElementById("performance").addEventListener("change", function() {
+    thumbDFunction.call(this, "performance");
+});
+document.getElementById("manual").addEventListener("change", function() {
+    thumbBFunction.call(this, "manual");
+});
+document.getElementById("perf").addEventListener("change", function() {
+    thumbAFunction.call(this, "perf");
+});
+document.getElementById("automation").addEventListener("change", function() {
+    thumbEFunction.call(this, "automation");
+});
+document.getElementById("infra").addEventListener("change", function() {
+    thumbBFunction.call(this, "infra");
+});
 
 // When a slider is changed by the user then this changes the colour of the thumb & track to correspond
 function thumbAFunction(argument) {
@@ -183,16 +220,4 @@ function thumbFFunction(argument) {
     } else if (x > 10) {
         this.className = "red";
     }
-}©
-2022 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
+}
